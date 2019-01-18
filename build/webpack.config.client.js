d5c78020e3d40ece216ee30a860221ce4c8a8c3f
cruxxxx <cruxxxx@126.com>
2019-01-18 14:03:51 +0800

    hot

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -35,19 +36,26 @@ const config = {
 }
 
 if(isDev){
+  config.entry = {
+    app:[
+      'react-hot-loader/patch',
+      path.join(__dirname, '../client/app.js')
+    ]
+  }
   config.devServer = {
     host: '0.0.0.0',
     port: '8888',
     contentBase: path.join(__dirname,'../dist'),
-    //hot: true,
+    hot: true,
     overlay: {
       error: true
     },//出错时页面弹窗提醒
-    publicPath: '/public',
+    publicPath: '/public/',
     historyApiFallback: {
       index: '/public/index.html'
     }
-  }
+  },
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 module.exports = config
